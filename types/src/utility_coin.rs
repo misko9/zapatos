1f7c159a0ed220458f8910ac1e047c30a76786b1
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-08-21 19:20:46 -0400

    [Types] rename coin not vendor-specific

--- types/src/utility_coin.rs
+++ types/src/utility_coin.rs
@@ -11,8 +11,8 @@ use once_cell::sync::Lazy;
 pub static APTOS_COIN_TYPE: Lazy<TypeTag> = Lazy::new(|| {
     TypeTag::Struct(Box::new(StructTag {
         address: AccountAddress::ONE,
-        module: ident_str!("aptos_coin").to_owned(),
-        name: ident_str!("AptosCoin").to_owned(),
+        module: ident_str!("gas_coin").to_owned(), //////// 0L //////// a number of API endpoint (e.g. simulate_gas) will check for the coin resource and are looking for a specific name, so we're changing this to the generic coin name
+        name: ident_str!("GasCoin").to_owned(), //////// 0L ////////
         type_params: vec![],
     }))
 });
